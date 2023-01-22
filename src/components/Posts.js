269f763ede683ddaa5ca1fd018f668d729de944f
shajjad Shagor <shajjadshagor@gmail.com>
2023-01-22 05:19:02 -0800

    design complete hide and show button and without api data design complete

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -6,6 +6,8 @@ const Posts = ({ posts, loading }) => {
     if(loading){
         return <h2>Loading....</h2>
     }
+    console.log(posts);
+
   return (
     <ul className='list-group my-4'>
         {
@@ -17,25 +19,70 @@ const Posts = ({ posts, loading }) => {
                                 <p>Name</p>
                             </div>
                             <div className="col">
-                                <h6>CONTACT</h6>
-                                <p>Get from API</p>
+                                <h6 className='fw-bold'>CONTACT</h6>
+                                <p className="fs-6">Get from API</p>
                             </div>
                             <div className="col">
-                                <h6>CITY</h6>
-                                <p>Get from API</p>
+                                <h6 className='fw-bold'>CITY</h6>
+                                <p className="fs-6">Get from API</p>
                             </div>
                             <div className="col">
-                                <h6>STATE</h6>
-                                <p>Get from API</p>
+                                <h6 className='fw-bold'>STATE</h6>
+                                <p className="fs-6">Get from API</p>
                             </div>
                             <div className='col'>
-                                <button type='button' className="btn btn-primary btn-sm" onClick={() => setShow(!show)}>
+                                <button type='button' className="btn btn-primary btn-sm rounded" onClick={() => setShow(!show)}>
                                     {show === true ? 'Hide Details' : 'View Details'}
                                 </button>
                             </div>
                             {show && (
-                                <div className=''>
-                                    <p>THis is test</p>
+                                <div className='container mt-4 mb-2 shadow-lg rounded'>
+                                    <div className="p-5">
+                                        <div className="row">
+                                            <div className="col">
+                                                <h6 className='fw-bold'>Description</h6>
+                                                <p className="fs-6">Data Get from API</p>
+                                            </div>
+                                        </div>
+                                        <div className="row mt-3">
+                                            <div className="col-5">
+                                                <div className=''>
+                                                    <h6 className="fw-bold">Contact Person</h6>
+                                                    <p className="fs-6">Data Get from API</p>
+                                                </div>
+                                                <div className=''>
+                                                    <h6 className="fw-bold">Designation</h6>
+                                                    <p className="fs-6">Data Get from API</p>
+                                                </div>
+                                                <div className=''>
+                                                    <h6 className="fw-bold">Emails</h6>
+                                                    <p className="fs-6">Data Get from API</p>
+                                                </div>
+                                                <div className=''>
+                                                    <h6 className="fw-bold">Phones</h6>
+                                                    <p className="fs-6">Data Get from API</p>
+                                                </div>
+                                            </div>
+                                            <div className="col-7">
+                                                <div className=''>
+                                                    <h6 className="fw-bold">Address</h6>
+                                                    <p className="fs-6">Data Get from API</p>
+                                                </div>
+                                                <div className=''>
+                                                    <h6 className="fw-bold">City</h6>
+                                                    <p className="fs-6">Data Get from API</p>
+                                                </div>
+                                                <div className=''>
+                                                    <h6 className="fw-bold">State</h6>
+                                                    <p className="fs-6">Data Get from API</p>
+                                                </div>
+                                                <div className=''>
+                                                    <h6 className="fw-bold">Country</h6>
+                                                    <p className="fs-6">Data Get from API</p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             )}
                         </div>
